14885adaff44c6f30292631dd88a94f8fc52b034
Joseph <jossosborne@gmail.com>
2018-02-10 21:41:13 -0800

    working update

--- loopArray.s
+++ loopArray.s
@@ -1,57 +1,41 @@
-@We will initialize table a, directly with the numbers, not in a loop as follows:
-@.data
-@.balign 4
-@
-@addr_of_a:
-@    .word 44
-@    .word 49
-@.....  make up 10 numbers
-@
-@.. change the loop from 100 to 40 (i.e. 40 bytes for 10 numbers)
-@Change the program in two parts: before the loop and in the loop.
-@
-@Before the loop:
-@   ldr r6, #0    /* initialize maximum to zero */
-@   ldr r5, MAX_VALUE /* load address of the variable MAX_ into r5
-@   str r6, [r5]   /* store value ZERO into the location of MAX_VAL
-@In the loop:
-@    ldr r5, MAX_VAL /* r5<- MAX_VALUE
-@    ldr r6, [r5]    /* r6<- value of MAX_VAL
-@    cmp r6, [r3]  /* compare scanned value in array against MAX_VA
-@    bls    NO_NEW_MAX /* no new max, skip code below */
-@    ldr r6, [r3]   /* found new max, load into r6
-@    str r6, [r5]  /* store the new max indirectly through r5
-@NO_NEW_MAX:  /* finish the loop as usual */
-
-MyArray: .skip 20 * 4
-
+@ initialize an array with ascii values A-Z by looping and then print by looping
+.data
+.balign 4
+MyArray: .skip 26 * 4 
 .text
 .global main
-
 main:
-        ldr     R0, =MyArray
-        mov     R1, #42
-        str     R1, [R0], #4
-        mov     R1, #43
-        str     R1, [R0], #4
-        mov r7, #1                      @ exit syscall
-        swi #0                          @ invoke syscall
-
-main:
-    mov r3, #0                      @ initialize loop counter
-print:
-    mov r0, #1                      @ STDOUT
-    ldr r1, addr_of_string          @ memory address of string
-    mov r2, #13                     @ size of string
-    mov r7, #4                      @ write syscall
-    swi #0                          @ invoke syscall
-loop:
-    cmp r3, #5                      @ compare counter to 5
-    beq exit                        @ exit loop if 5
-    add r3, r3, #1                  @ increment counter
-    b print                         @ loop print
+        ldr     r6, addr_of_MyArray		@ load &MyArray r0 (base addr)
+   	mov	r2, #0x40			@ generated values to store
+	eor 	r5, r5				@ counter
+	eor	r3, r3				@ ARRAY OFFSET 
+	str	r2, [r6]			@ store 0 as first value 	
+arr_setup:		
+	add	r2, r2, #0x01			@ generate value to store
+   	add	r3, r6, r5, LSL#2		@ new ARRAY OFFSET = (bp + (counter * 4)) 	
+	str     r2, [r3]			@ store values into array addr
+	add	r5, #1				@ inc loop counter
+	cmp	r5, #26				@ cmp, loop 20 times	
+	bne	arr_setup			@ loop until i = 26
+	eor	r5, r5				@ reset counter and offset calculation	
+print_arr:	
+	add	r3, r6, r5, LSL#2		@ calc new array offset = (bp + (counter * 4)) 		
+	mov 	r0, #1				@ STDOUT
+	mov	r1, r3				@ load addr	
+	mov 	r2, #1				@ size to write
+	mov 	r7, #4				@ syscall write
+	svc 	0				@ invoke syscall (bl write)	
+	mov 	r0, #1				@ STDOUT
+	ldr	r1, nl_char_addr		@ load new line addr
+	mov 	r2, #1				@ size to write
+	mov 	r7, #4				@ syscall write
+	svc 	0				@ invoke syscall (bl write)		
+	add 	r5, r5, #1			@ increment counter
+	cmp 	r5, #26 			@ loop 26 times
+	bne	print_arr			@ loop print array ascii and new line
 exit:
-    mov r7, #1                      @ exit syscall
-    swi #0                          @ invoke syscall
-
-addr_of_string: .word string
+        mov r7, #1                              @ exit syscall
+        svc 0	                               	@ invoke syscall
+addr_of_MyArray: .word MyArray
+nl_char: .ascii "\n"
+nl_char_addr: .word nl_char
